28dce5379b2c0be78f1ce8f29c9e60109feffe4d
ek3878 <ek3878@nyu.edu>
2024-03-15 14:55:59 +0800

    upload youtubezip

---
+++ YoutubeProject/Youtube-Project/src/main/java/com/youtubegroup/Youtube/Project/YoutubeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.youtubegroup.Youtube.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeProjectApplication.class, args);
+	}
+
+}
---
+++ YoutubeProject/Youtube-Project/src/test/java/com/youtubegroup/Youtube/Project/YoutubeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubegroup.Youtube.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
