6267d9e32b20eadc71b68e19fd80ef5bced34345
LuluLizzy <113492524+LuluLizzy8@users.noreply.github.com>
2024-03-16 12:59:24 +0800

    Another Version of Youtube CLone
    I created maven modules for Backend and Frontend. Try opening this project in eclipse or intellij. It should work

---
+++ YoutubeClone2/Backend/src/test/java/com/youtubeproject/Youtube/Clone/YoutubeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubeproject.Youtube.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
